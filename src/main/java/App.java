9b4b9ed5ad6745d92c3a5ed0da92232da654cedc
JuneMi42 <xela7117@gmail.com>
2022-08-22 02:19:33 +0300

    Fist_try

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHelloWorld1 = applicationContext.getBean("helloWorld", HelloWorld.class);
+        System.out.println(beanHelloWorld1.getMessage());
+        HelloWorld beanHelloWorld2 = applicationContext.getBean("helloWorld", HelloWorld.class);
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(beanHelloWorld1 == beanHelloWorld2);
+        System.out.println(catBean1 == catBean2);
+
+
     }
 }
